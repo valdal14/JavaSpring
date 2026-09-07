11058141519ba0fdc7b89fe7f445ae2283ae1194
Valerio DAlessio <u105550@canda.com>
2026-09-07 10:11:18 +0200

    Initial Project Config

--- SpringBeans/src/main/java/com/vd14/springbean/App.java
+++ SpringBeans/src/main/java/com/vd14/springbean/App.java
@@ -2,6 +2,6 @@ package com.vd14.springbean;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Spring Beans");
     }
 }
