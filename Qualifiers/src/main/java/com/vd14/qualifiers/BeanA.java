90c77d9c4d114a5b2088e684e1347f07d95de624
Valerio DAlessio <u105550@canda.com>
2026-09-10 16:01:53 +0200

    Add qualifier to help Spring to inject the related bean

--- Qualifiers/src/main/java/com/vd14/qualifiers/BeanA.java
+++ Qualifiers/src/main/java/com/vd14/qualifiers/BeanA.java
@@ -1,4 +1,17 @@
 package com.vd14.qualifiers;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+@Getter
 public class BeanA {
+    private BeanB beanB;
+
+    @Autowired
+    @Qualifier(value = "beanB2Qualifier")
+    public void setBeanB(BeanB beanB) {
+        System.out.println("setBeanB: " + beanB);
+        this.beanB = beanB;
+    }
 }
