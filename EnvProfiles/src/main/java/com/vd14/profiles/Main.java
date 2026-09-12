58fd63fdff5f43adc2444d8546bd65f1bdc867ea
Valerio DAlessio <u105550@canda.com>
2026-09-12 19:12:46 +0200

    Add profiles to manage different envs

--- EnvProfiles/src/main/java/com/vd14/profiles/Main.java
+++ EnvProfiles/src/main/java/com/vd14/profiles/Main.java
@@ -3,10 +3,15 @@ package com.vd14.profiles;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml");
-        BeanA beanA = context.getBean("beanDeve",  BeanA.class);
+        BeanA beanA = context.getBean("beanA",  BeanA.class);
         beanA.getStage();
+
+        String[] activeProfiles = context.getEnvironment().getActiveProfiles();
+        System.out.println("Active Profiles: " + Arrays.toString(activeProfiles));
     }
 }
